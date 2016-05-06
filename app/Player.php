718d638221f9758e597ab0e7de79be4cd70451c8
Anthony Serino <mrkordell@gmail.com>
2016-05-06 09:52:06 -0400

    Applied fixes from StyleCI

--- app/Player.php
+++ app/Player.php
@@ -6,6 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-  public $guarded = array('id');
-
+    public $guarded = ['id'];
 }
--- database/migrations/2016_04_28_165335_CreatePlayersTable.php
+++ database/migrations/2016_04_28_165335_CreatePlayersTable.php
@@ -1,15 +1,15 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreatePlayersTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+      * Run the migrations.
+      *
+      * @return void
+      */
      public function up()
      {
          Schema::create('players', function (Blueprint $table) {
